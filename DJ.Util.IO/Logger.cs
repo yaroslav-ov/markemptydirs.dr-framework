9a7e55d08607672457f5f96666fe91b8bc52a1bc
Jonny Dee <jonny.dee@gmx.net>
2009-07-25 18:02:31 +0200

    - Added advanced option parsing mechanism. - Introduced Logger.

---
+++ DJ.Util.IO/Logger.cs
@@ -0,0 +1,53 @@
+//  Copyright (C) 2009 by Johann Duscher (alias Jonny Dee)
+//
+//  This file is part of MarkEmptyDirs.
+//
+//  MarkEmptyDirs is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  MarkEmptyDirs is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with MarkEmptyDirs.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+
+namespace DJ.Util.IO
+{
+    public static class Logger
+    {
+        public enum LogType
+        {
+            Debug,
+            Info,
+            Error
+        }
+
+        public static void Log(Exception ex)
+        {
+            Log(LogType.Error, ex.Message);
+        }
+        
+        public static void Log(LogType type, string message)
+        {
+            switch (type)
+            {
+                case LogType.Debug:
+                    Console.Out.WriteLine("DEBUG: " + message);
+                    break;
+                case LogType.Info:
+                    Console.Out.WriteLine("INFO: " + message);
+                    break;
+                case LogType.Error:
+                    Console.Error.WriteLine("ERROR: " + message);
+                    break;
+            }
+            
+        }        
+    }
+}
---
+++ DJ.Util.IO/OptionDescriptor.cs
@@ -0,0 +1,40 @@
+//  Copyright (C) 2009 by Johann Duscher (alias Jonny Dee)
+//
+//  This file is part of MarkEmptyDirs.
+//
+//  MarkEmptyDirs is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  MarkEmptyDirs is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with MarkEmptyDirs.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+
+namespace DJ.Util.IO
+{
+    
+    public class OptionDescriptor
+    {
+        public string Name { get { return LongNames[0]; } }
+        public string[] LongNames { set; get; }
+        public char[] ShortNames { set; get; }
+        public string ShortDescription { set; get; }
+        public string LongDescription { set; get; }
+        public bool CanHaveValue { set; get; }
+        public bool MandatoryValue {set; get; }
+
+        public override string ToString ()
+        {
+            
+            return string.Format("[OptionDescriptor: LongNames={0}, ShortNames={1}, ShortDescription={2}, LongDescription={3}, CanHaveValue={4}, MandatoryValue={5}]", string.Join("|", LongNames), new string(ShortNames), ShortDescription, LongDescription, CanHaveValue, MandatoryValue);
+        }
+    }
+
+}
