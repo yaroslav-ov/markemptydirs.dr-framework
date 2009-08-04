cfb1d784c5aaf8debc8de647cee34e6f3fb62f03
Johann Duscher <johann.duscher@messe-muenchen.de>
2009-08-04 12:35:10 +0200

    Changed platform dependent code into platform independent code.

---
+++ DJ.Util/IO/PathUtil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace DJ.Util.IO
+{
+    public static class PathUtil
+    {
+        public static string Combine(params string[] pathComponents)
+        {
+            if (null == pathComponents || pathComponents.Length == 0)
+                throw new ArgumentException("At least one path component must be provided", "pathComponents");
+
+            var path = pathComponents[0];
+
+            for (var i = 1; i < pathComponents.Length; i++)
+                path = Path.Combine(path, pathComponents[i]);
+
+            return path;
+        }
+    }
+}
